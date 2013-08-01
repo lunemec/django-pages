6ca39ad10afbdfb788d53f9d14e16e363576d22f
Lukas Nemec <lu.nemec@gmail.com>
2013-08-01 09:35:19 +0200

    changed todo, log does not need changing

--- django_pages/log/middleware.py
+++ django_pages/log/middleware.py
@@ -1,10 +1,11 @@
-import datetime
+# -*- encoding: utf-8 -*-
+
 import logging
 
 
 class RequestLog(object):
 
     def process_request(self, request):
-        
+
         log = logging.getLogger('requestlog')
         log.info('REQUEST: %s' % unicode(request).replace('\n', ' ').encode('utf-8'))
