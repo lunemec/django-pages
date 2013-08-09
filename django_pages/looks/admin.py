bd2c008e392e7252f7e9b04c57a6f8573cb033f9
Lukas Nemec <lu.nemec@gmail.com>
2013-08-09 11:32:31 +0200

    removed lightbox, commited default3 style, fixed template bug

--- django_pages/looks/admin.py
+++ django_pages/looks/admin.py
@@ -2,8 +2,10 @@
 
 import reversion
 
+from ..common.admin_actions import activate, deactivate
 
 class LooksAdmin(reversion.VersionAdmin):
 
     fields = ('template', 'posts_per_page', 'active')
     list_display = fields
+    actions = [activate, deactivate]
--- django_pages/static/admin/js/ckeditor/config.js
+++ django_pages/static/admin/js/ckeditor/config.js
@@ -7,5 +7,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	 config.language = 'en';
 	// config.uiColor = 'AADC6E';
-    config.extraPlugins = 'slimbox';
+//    config.extraPlugins = 'slimbox';
 };
